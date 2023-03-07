0edd783f35d9e7f2a787b1220ac9e3bebd07506f
harzyz <abdulazeezahmed85@gmail.com>
2023-03-07 00:38:27 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function myBlue() {
+    document.getElementById('squareBox').style.backgroundColor = 'blue'
+}
+
+function myRed() {
+    document.getElementById('squareBox').style.backgroundColor = 'red'
+}
+
+function myGreen() {
+    document.getElementById('squareBox').style.backgroundColor = 'green'
+}
+
+
